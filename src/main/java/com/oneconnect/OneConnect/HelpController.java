034d975ecc582d7db35d46dca1b0a4b8e66f140c
Stephen Rose <rosesd97@yahoo.com>
2020-03-26 11:47:19 -0400

    File creation and code base completed: User, Child, UserCreationService Users.json updated ID naming UserCreationController created

--- src/main/java/com/oneconnect/OneConnect/HelpController.java
+++ src/main/java/com/oneconnect/OneConnect/HelpController.java
@@ -1,9 +1,8 @@
 package com.oneconnect.OneConnect;
 
-<<<<<<< HEAD
-=======
+
 import com.oneconnect.OneConnect.Course.CourseService;
->>>>>>> 4e83656bd26f6ba62a1633e772987386b405f2ca
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,12 +15,9 @@ public class HelpController {
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView getHelp(@RequestParam String role) {
-<<<<<<< HEAD
 
         System.out.println("sending a help page");
 
-=======
->>>>>>> 4e83656bd26f6ba62a1633e772987386b405f2ca
         ModelAndView modelAndView = new ModelAndView();
         switch (role) {
             case ("parent"):
